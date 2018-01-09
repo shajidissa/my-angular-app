675200f7a6a3542b417c3cbb7af8e0930ab40d20
Shajid Issa <saj.issa@gmail.com>
2018-01-09 15:26:55 +0000

    home description

--- src/main/resources/src/app/home-description.component.ts
+++ src/main/resources/src/app/home-description.component.ts
@@ -11,10 +11,36 @@ import { Observable } from 'rxjs/Observable';
   <home-header></home-header>
   <br>
   <div class="container">
-    This is a simple Oauth2 Angular4, Spring Microservices example.
+    This is a simple Oauth2, Angular4, Spring Microservices example.
+    <br>
+    <br>
+    You can click on Actions; View Patients (GET), Add a Patient (POST), Find/Edit/Delete a Patient (GET/PUT/DELETE)
+    <br>
+    <br>
+    A description of the Rest api can be found in the Swagger link.
     <br>
     <br>
     The Spring discovery service Eurareka is : <a href="http://51.141.9.85:8761" target="_blank">http://51.141.9.85:8761</a>
+    <br>
+    <br>
+    Jenkins pipe line : <a href="http://52.191.209.219/jenkins/" target="_blank">http://52.191.209.219/jenkins/</a>
+    <br>
+    <br>
+    Github repos :
+    <br>
+    <a href="https://github.com/shajidissa/my-angular-app" target="_blank">my-angular-app</a>
+    <br>
+    <a href="https://github.com/shajidissa/my-patient-microservice" target="_blank">my-patient-microservice</a>
+    <br>
+    <a href="https://github.com/shajidissa/my-eureka-server" target="_blank">my-eureka-server</a>
+    <br>
+    <a href="https://github.com/shajidissa/my-zuul-server" target="_blank">my-zuul-server</a>
+    <br>
+    <a href="https://github.com/shajidissa/my-authenticating-service" target="_blank">my-authenticating-service</a>
+    <br>
+    <a href="https://github.com/shajidissa/my-configuration-server" target="_blank">my-configuration-server</a>
+    <br>
+    <a href="https://github.com/shajidissa/config-repo" target="_blank">config-repo</a>
     
   </div>
 
